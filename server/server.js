960a0a2130fa9e3acb6d5972e41dd0260ccfebae
divyansh <bt22cse003@iiitn.ac.in>
2025-05-03 03:39:53 +0530

    fix: set ports and hosts for prod

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.get('*', (req, res) => {
     res.sendFile(indexPath);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
